7a55789e831171c3fb13b0c6ffcd0b8c21219412
mgltorsa <mgltorsa@gmail.com>
2020-06-23 02:40:15 -0500

    added expression's basics

---
+++ src/main/java/co/edu/icesi/metrocali/evaluator/expression/SpringExpressions.java
@@ -0,0 +1,23 @@
+package co.edu.icesi.metrocali.evaluator.expression;
+
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringExpressions {
+    private ExpressionParser parser = new SpelExpressionParser();
+    private StandardEvaluationContext context=new StandardEvaluationContext();
+
+
+    public void setVariable(String name, Object val){
+        context.setVariable(name, val);
+    }
+    public Object parseExpression(String expression){
+        return parser.parseExpression(expression).getValue(context);
+    }
+    public void setRootObject(Object root){
+        context.setRootObject(root);
+    }
+}
