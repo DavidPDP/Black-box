b9fac1d3b7a28b8841d732d41fef7bf14c94d745
mgltorsa <mgltorsa@gmail.com>
2020-06-26 14:54:19 -0500

    added custom queries

--- src/main/java/co/edu/icesi/metrocali/blackbox/repositories/evaluator/FormulasRepository.java
+++ src/main/java/co/edu/icesi/metrocali/blackbox/repositories/evaluator/FormulasRepository.java
@@ -1,6 +1,7 @@
 package co.edu.icesi.metrocali.blackbox.repositories.evaluator;
 
 import java.util.List;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import co.edu.icesi.metrocali.blackbox.entities.evaluator.Formula;
@@ -17,4 +18,10 @@ public interface FormulasRepository extends CrudRepository<Formula, Integer> {
     public List<Formula> findAll();
 
     public void findByEndDateIsNull();
+
+    @Query(value = "SELECT f FROM Formula f, Variable v WHERE f.variable = v AND v.isKPI = TRUE")
+    public List<Formula> findByKPIs();
+
+    @Query(value = "SELECT f FROM Formula f, Variable v WHERE f.endDate IS NULL AND f.variable = v AND v.isKPI = TRUE")
+    public List<Formula> findActivesByKPIs();
 }
