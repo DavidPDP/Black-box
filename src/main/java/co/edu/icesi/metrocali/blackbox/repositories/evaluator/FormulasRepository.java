d634817fb1f147d3ebca522298c3e574e3163db2
mgltorsa <mgltorsa@gmail.com>
2020-06-26 20:01:51 -0500

    added findActiveByVariable method

--- src/main/java/co/edu/icesi/metrocali/blackbox/repositories/evaluator/FormulasRepository.java
+++ src/main/java/co/edu/icesi/metrocali/blackbox/repositories/evaluator/FormulasRepository.java
@@ -3,6 +3,7 @@ package co.edu.icesi.metrocali.blackbox.repositories.evaluator;
 import java.util.List;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import co.edu.icesi.metrocali.blackbox.entities.evaluator.Formula;
 import co.edu.icesi.metrocali.blackbox.entities.evaluator.Variable;
@@ -24,4 +25,7 @@ public interface FormulasRepository extends CrudRepository<Formula, Integer> {
 
     @Query(value = "SELECT f FROM Formula f, Variable v WHERE f.endDate IS NULL AND f.variable = v AND v.isKPI = TRUE")
     public List<Formula> findActivesByKPIs();
+
+    @Query(value = "SELECT f FROM Formula f, Variable v WHERE f.variable.nameVariable = :variable_name AND f.endDate IS NULL")
+    public Formula findActiveByVariable(@Param("variable_name") String variableName);
 }
